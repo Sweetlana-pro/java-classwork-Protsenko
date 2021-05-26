b7a05be2641bfe13e05b49c65298b40e03f50626
Sweetlana Protsenko <svitprotsenko@gmail.com>
2021-05-26 14:07:57 -0600

    HelloWorld in NetBeans

---
+++ HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * @author Sweetlana Protsenko
+ * email: svitprotsenko@gmail.com
+ * date: May24,2021
+ * purpose: My first Hello World program in an IDE
+ */
+
+package com.mthree.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Lana!");
+    }
+}
