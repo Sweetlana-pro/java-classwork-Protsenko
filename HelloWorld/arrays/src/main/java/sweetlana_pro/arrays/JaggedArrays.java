aa0abf79bd8de23e7d51a4a73f75958b5c9c2407
Sweetlana Protsenko <svitprotsenko@gmail.com>
2021-06-05 17:58:58 -0600

    arrays

---
+++ HelloWorld/arrays/src/main/java/sweetlana_pro/arrays/JaggedArrays.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sweetlana_pro.arrays;
+
+/**
+ *
+ * @author Sweetlana Protsenko
+ */
+public class JaggedArrays {
+    public static void main(String[] args) {
+        //int[] numbers = {3,5, 2, 1};
+        //int sum = 0;
+            //for (int i = 0; i < numbers.length; i++) {
+                //sum += numbers[i];
+                //System.out.println("i = " + i + " current sum = " + sum);
+            //}
+        //System.out.println("Final sum: " + sum);
+        
+        //int[] num = {3, 5, 2, 0};
+        //for (int a = num.length - 1; a >=0; a--){
+            //if (a % 2 == 1) {
+                //System.out.println("index " + a + " - " + num[a]);
+            //}
+        //}
+        //int[] numb = {1, 2, 3, 4, 5, 6};
+        //for (int b = 0; b < numb.length - 2; b += 3) {
+            //System.out.println("Pair: (" + numb[b]+ ", " + numb[b + 1] + ")");
+        //}
+        //public static int[] GrowArray(int[] original, int howManyMoreElements){ 
+    
+            //int[] newArray = new int [original.lenth + howManyMoreElements];
+            //for (int c = 0; c < original.length; c++)
+            //{ 
+                //newArray[c] = original[c];
+            //} 
+            //return newArray;
+        }
+    
+}
